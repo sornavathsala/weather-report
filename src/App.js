da83fac38e240bd5d714dac4a46d9263a154d1a9
Sorna <72265407+sornavathsala@users.noreply.github.com>
2022-11-15 11:47:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import Button from 'react-bootstrap/Button';
-// import InputGroup from 'react-bootstrap/InputGroup';
-// import FormControl from 'react-bootstrap/FormControl';
-// import ListGroup from 'react-bootstrap/ListGroup'; 
-
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+
+import { BsFillSunFill, BsSnow2} from 'react-icons/bs';
+import { WiCelsius } from 'react-icons/wi'
 
 function App() {
 
-const f = 150;
-let c= (f-32) * 5/9;
+  const apiKey = '8753ed41c51a102a72a3ac7f1e21da01';
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+  const [fahrenheit, setFahrenheit] = useState("");
+  const [celsius, setCelsius] = useState("");
+  let c;
+  const getWeather = (e) =>{
+    if(e.key === 'Enter'){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+      .then( response=> response.json())
+      .then(
+        data => {
+          setWeatherData(data);
+          setCity("");
+          setCelsius("");
+        }
+      )
+    }
+  }
+  const fToc = () =>
+  {
+     c = ((weatherData.main.temp)-32) * 5/9
+     setCelsius (c.toFixed(2));  
 
-if((c < 70)
-?c =  <p style={{color:"green"}}>{c.toFixed(2)}</p> : c = <p style={{color:"red"}}>{c.toFixed(2)}</p>);
+     if(((weatherData.main.temp) < 70)
+     ? setFahrenheit(<p style={{color:"green"}}>{Math.round(weatherData.main.temp)}ºF  <BsSnow2/></p>)
+     : setFahrenheit(<p style={{color:"red"}}>{Math.round(weatherData.main.temp)}ºF<BsFillSunFill/></p>));
+  }
+  const color = () =>{
 
+  }
 const noOfDay = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
 
 // day
@@ -31,26 +51,52 @@ const noOfDay = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY
   if ((hour === 0)
   ? (hour = 12)
   : ((hour > 12)
-  ? (hour ="0" + hour - 12 ) && (period= "PM") : (hour = ("0" + hour))));
+  ? (hour ="0" + hour - 12 ) && (period= "PM")
+  : (hour = ("0"+ hour))));
 
 //min
   let mins = d.getMinutes();
 
-  // if((mins < 10)? mins = "0"+mins )
 return (
 
 <div className="App">
-  <div className="App-header">
-  <div className="day">
-    {today} <span style={{margin:"40px"}}>{hour}:{mins} {period}</span>
-  </div>
+  <div className="body">  
+    <div className="App-header">
+        <div className="day">
+          {today}
+          <span style={{margin:"50px",fontSize:"20px"}}>
+            {hour}:{mins} {period}
+          </span>
+        </div>
+    </div>
+        <input 
+      className="input"
+      placeholder="Enter City Name..."
+      onChange={e=>{setCity(e.target.value)}}
+      value={city}
+      onKeyPress = {getWeather}/>
 
-  <div className="weather">
-    {c}<span>C</span>
-  </div>
-  <button className="btn">Convert</button>
+    <div>
+      {typeof weatherData.main === 'undefined' 
+        ? (
+          <div>
+            <p  className="welcome">Welcome to Weather App</p>            
+
+          </div>
+          ) : (
+          <div className="weather">
+            <p>{weatherData.name}</p>
+              <p>{fahrenheit}</p>
+                {celsius}
+
+        </div>)
+      }        <button  className="button" onClick={fToc}>Convert</button>
+          <div style={{font_size:"30"}}>
+    </div>
+
+    </div>
+  </div>          
 </div>
-  </div>
 
 );
 }
